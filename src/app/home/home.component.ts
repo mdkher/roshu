f6a7d286faeb1179dcfd23e91c50175308d60cf2
Manan Kher <mdkher@gmail.com>
2021-02-03 00:15:45 +0530

    Initial push

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  tryout(){
+    var x = document.getElementById('click');
+    x.style.opacity = "0";
+  }
+
+}
