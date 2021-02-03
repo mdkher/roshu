ac89304658d6e866e7f9172d0ec174d1a8e93c6d
Manan Kher <mdkher@gmail.com>
2021-02-03 10:23:06 +0530

    changes Pushed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  top: any[] = [
+    {value: '../../assets/Asset 2.svg', viewValue: 'Blue'},
+    {value: '../../assets/Asset 4.svg', viewValue: 'Pink'}
+  ];
+  bottom: any[] = [
+    {value: '../../assets/Asset 6.svg', viewValue: 'Pink'},
+    {value: '../../assets/Asset 7.svg', viewValue: 'Yellow'}
+  ];
+
+  topImage;
+  bottomImage;
+
+  selectedTop = this.top[0].value;
+  selectedBottom = this.bottom[0].value;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.topImage = "../../assets/Asset 2.svg";
+    this.bottomImage = "../../assets/Asset 6.svg";
+  }
+
+  valueChangeTop(data: any){
+    this.topImage = data;
+    console.log(this.topImage);
+  }
+  valueChangeBottom(data: any){
+    this.bottomImage = data;
+    console.log(this.bottomImage);
   }
 
 }
