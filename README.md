a695fce1f2f9cb8b5c7e0552c363cbba525a74bd
Manan Kher <mdkher@gmail.com>
2021-02-03 08:45:29 +0530

    modified changes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Roshu1
+# roshu
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.1.2.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'roshu1'`, () => {
+  it(`should have as title 'roshu'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('roshu1');
+    expect(app.title).toEqual('roshu');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('roshu1 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('roshu app is running!');
   });
 });
