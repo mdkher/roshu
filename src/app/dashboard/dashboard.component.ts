b6ef7e1f5bac792ae631925cc49612c1a99674b2
Manan Kher <mdkher@gmail.com>
2021-02-03 10:48:25 +0530

    modified images path

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,17 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
 
   top: any[] = [
-    {value: 'assets/Asset_2.svg', viewValue: 'Blue'},
-    {value: 'assets/Asset_4.svg', viewValue: 'Pink'}
+    {value: 'assets/images/Asset_2.svg', viewValue: 'Blue'},
+    {value: 'assets/images/Asset_4.svg', viewValue: 'Pink'}
   ];
   bottom: any[] = [
-    {value: 'assets/Asset_6.svg', viewValue: 'Pink'},
-    {value: 'assets/Asset_7.svg', viewValue: 'Yellow'}
+    {value: 'assets/images/Asset_6.svg', viewValue: 'Pink'},
+    {value: 'assets/images/Asset_7.svg', viewValue: 'Yellow'}
   ];
 
   topImage;
   bottomImage;
-  faceImage = "assets/Asset_3.svg";
+  faceImage = "assets/images/Asset_3.svg";
 
   selectedTop = this.top[0].value;
   selectedBottom = this.bottom[0].value;
@@ -26,8 +26,8 @@ export class DashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.topImage = "assets/Asset_2.svg";
-    this.bottomImage = "assets/Asset_6.svg";
+    this.topImage = "assets/images/Asset_2.svg";
+    this.bottomImage = "assets/images/Asset_6.svg";
   }
 
   valueChangeTop(data: any){
