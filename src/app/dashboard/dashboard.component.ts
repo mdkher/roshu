76e0c401fc7409ee96f964fb34b72be2e68ed804
Manan Kher <mdkher@gmail.com>
2021-02-13 10:17:25 +0530

    modified code

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,12 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
 
   top: any[] = [
-    {value: 'assets/images/top1.svg', viewValue: 'top/1'},
-    {value: 'assets/images/top2.svg', viewValue: 'top/2'},
-    {value: 'assets/images/top3.svg', viewValue: 'top/3'},
+    { value: 'assets/images/top1.svg', viewValue: 'Eating' },
+    { value: 'assets/images/top2.svg', viewValue: 'Bitcoin' },
+    { value: 'assets/images/top3.svg', viewValue: 'Stick Walking' },
   ];
   bottom: any[] = [
-    {value: 'assets/images/bottom-1.svg', viewValue: 'bottom-1'},
+    { value: 'assets/images/bottom-1.svg', viewValue: 'Yellow' },
   ];
 
   topImage = this.top[0].value;
@@ -25,15 +25,15 @@ export class DashboardComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
-  valueChange(data: any, top: boolean){
-    if(top) {
+  valueChange(data: any, top: boolean) {
+    if (top) {
       this.topImage = data;
     } else {
       this.bottomImage = data;
     }
   }
-  
+
 
 }
